5bb4dd480a00fd6ce05ee42716542b99ec3fa169
Gaurav <gaurav4.kumar@upgrad.com>
2023-06-24 06:09:57 +0530

    counter updated

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,20 +1,20 @@
 import classes from './Auth.module.css';
 import { useSelector, useDispatch } from 'react-redux';
 
-import { authActions } from '../store';
+import { authActions } from '../store/index';
 
 const Auth = () => {
   const dispatch = useDispatch();
-  const auth = useSelector((state) => state.authentication)
 
-  const loginHandler = () => {
-
+  const loginHandler = (event) => {
+    event.preventDefault();
+    dispatch(authActions.login());
   }
 
   return (
     <main className={classes.auth}>
       <section>
-        <form>
+        <form onSubmit = {loginHandler}>
           <div className={classes.control}>
             <label htmlFor='email'>Email</label>
             <input type='email' id='email' />
@@ -23,7 +23,7 @@ const Auth = () => {
             <label htmlFor='password'>Password</label>
             <input type='password' id='password' />
           </div>
-          <button onClick = {loginHandler} >Login</button>
+          <button >Login</button>
         </form>
       </section>
     </main>
--- src/store/index.js
+++ src/store/index.js
@@ -13,34 +13,34 @@ const counterSlice = createSlice({
             state.counter--;
         },
         increase(state, action) {
-            state.counter + action.payload;
+            state.counter = state.counter + action.payload;
         },
         toggleCounter(state) {
             state.showCounter = !state.showCounter;
-        }
+        },
 
     }
 });
 
 const initialAuthState = {
-    isAuthenticated: false
+    isAuthenticated: false,
 }
 
 const authSlice = createSlice({
     name: 'authentication',
     initialState: initialAuthState,
     reducers: {
-        login(state, action) {
+        login(state) {
             state.isAuthenticated = true;
         },
         logout(state) {
             state.isAuthenticated = false;
-        }
-    }
-})
+        },
+    },
+});
 
 const store = configureStore({
-    reducer: {counter: counterSlice.reducer, auth: authSlice.reducer}
+    reducer: {counter: counterSlice.reducer, auth: authSlice.reducer},
 });
 
 export const counterActions = counterSlice.actions;
